6d1eab19b3f438c2e51145d6e736baac4dbe6b4f
crimsonthunder54 <crimsonthunder54@gmail.com>
2016-02-20 14:59:28 +0100

    fix build

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,7 @@ export ARCH=arm
 export CROSS_COMPILE=/home/crimsonthunder/arm-eabi-5.2/bin/arm-eabi-
 mkdir output
 
-make -C $(pwd) O=$(pwd)/output VARIANT_DEFCONFIG=apq8084_sec_trlte_eur_defconfig apq8084_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig SELINUX_LOG_DEFCONFIG=selinux_log_defconfig TIMA_DEFCONFIG=tima_defconfig DMVERITY_DEFCONFIG=dmverity_defconfig
+make -C $(pwd) O=$(pwd)/output VARIANT_DEFCONFIG=apq8084_sec_trlte_eur_defconfig apq8084_sec_defconfig
 make -C $(pwd) O=output -j12
 
 mv output/arch/arm/boot/zImage $(pwd)/imgtools/Boot/kernel
