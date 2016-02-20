87cab0652e8a2679eb89043f5ad98834d78623a8
crimsonthunder54 <crimsonthunder54@gmail.com>
2016-02-20 23:25:54 +0100

    Revert "cpufreq: force cpuN policy to match cpu0 [imoseyon & twistedumbrella]"
    This reverts commit 25a906732dd27aa77e87d38b9b30e97a7f028f93.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1964,7 +1964,6 @@ int cpufreq_driver_target(struct cpufreq_policy *policy,
 }
 EXPORT_SYMBOL_GPL(cpufreq_driver_target);
 
-
 int __cpufreq_driver_getavg(struct cpufreq_policy *policy, unsigned int cpu)
 {
     int ret = 0;
@@ -2155,7 +2154,6 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 {
 	int ret = 0, failed = 1;
 	struct cpufreq_policy *cpu0_policy = cpufreq_cpu_get(0);
-
 	pr_debug("setting new policy for CPU %u: %u - %u kHz\n", new_policy->cpu,
 		new_policy->min, new_policy->max);
 
@@ -2191,14 +2189,8 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	/* notification of the new policy */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_NOTIFY, new_policy);
-
-	if (policy->cpu >= 1) {
-		policy->min = cpu0_policy->min;
-		policy->max = cpu0_policy->max;
-	} else {
-        policy->min = new_policy->min;
-        policy->max = new_policy->max;
-	}
+	policy->min = new_policy->min;
+	policy->max = new_policy->max;
 
 	pr_debug("new min and max freqs are %u - %u kHz\n",
 					policy->min, policy->max);
@@ -2224,11 +2216,7 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 			}
 
 			/* start new governor */
-		if (policy->cpu >= 1 && cpu0_policy) {
-                    policy->governor = cpu0_policy->governor;
- 	        } else {
-                    policy->governor = new_policy->governor;
-                }
+			policy->governor = new_policy->governor;
 			if (!__cpufreq_governor(policy, CPUFREQ_GOV_POLICY_INIT)) {
 				if (!__cpufreq_governor(policy, CPUFREQ_GOV_START)) {
 					failed = 0;
